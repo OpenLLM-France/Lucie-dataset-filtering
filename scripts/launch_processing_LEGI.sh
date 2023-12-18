85cabbb6d92a791577110659a865112f0c1935ff
Evan Dufraisse <edufraisse@gmail.com>
2023-12-18 17:25:59 +0100

    Remove commented out code in launch_processing_LEGI.sh

--- scripts/launch_processing_LEGI.sh
+++ scripts/launch_processing_LEGI.sh
@@ -8,67 +8,6 @@ eval "$(conda shell.bash hook)"
 
 conda activate /scratch/envs/conda/redpajama
 
-    # parser.add_argument(
-    #     "--path_dataset",
-    #     type=str,
-    #     help="Path to the dataset to process",
-    # )
-    # parser.add_argument(
-    #     "--path_output",
-    #     type=str,
-    #     help="Path to the output folder",
-    # )
-    # parser.add_argument(
-    #     "--path_fasttext_model",
-    #     type=str,
-    #     help="Path to the fasttext model",
-    # )
-    # parser.add_argument(
-    #     "--path_perplexity_models",
-    #     type=str,
-    #     help="Path to the perplexity models",
-    # )
-    # parser.add_argument(
-    #     "--path_words_filter",
-    #     type=str,
-    #     help="Path to the words filter",
-    # )
-    # parser.add_argument(
-    #     "--path_domain_filter",
-    #     type=str,
-    #     help="Path to the domain filter",
-    # )
-    # parser.add_argument(
-    #     "--path_cut_offs",
-    #     type=str,
-    #     help="Path to the cut offs",
-    # )
-    # parser.add_argument(
-    #     "--mapping_fields",
-    #     type=str,
-    #     help="Mapping fields",
-    # )
-    # parser.add_argument(
-    #     "--default_fields",
-    #     type=str,
-    #     help="Default fields",
-    # )
-    # parser.add_argument(
-    #     "--fields_to_keep",
-    #     type=str,
-    #     help="Fields to keep",
-    # )
-    # parser.add_argument(
-    #     "--language",
-    #     type=str,
-    #     help="Language",
-    # )
-    # parser.add_argument(
-    #     "--n_processes",
-    #     type=int,
-    #     help="Number of processes",
-    # )
-
 # 'url' possible pour mapping_fields
 
 python ${CODE_DIR}/Bloom-ng-dataset-processing/src/blmrdata/utils/redpajama/worker.py \
