c41a4db3690d6954580b3888c87d5446c0cc2ad6
Evan DUFRAISSE <evan.dufraisse@cea.fr>
2024-02-20 13:41:46 +0100

    Add job_fai2.sh script for preprocessing dataset

---
+++ scripts/job_fai2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --account=ksy@cpu
+#SBATCH --job-name=Preprocess
+#SBATCH --ntasks=1
+#SBATCH --time=20:00:00
+#SBATCH -c 40
+#SBATCH --hint=nomultithread
+#SBATCH --partition=cpu_p1
+
+eval "$(conda shell.bash hook)"
+
+conda activate redpajama
+
+folder_dataset=$1
+
+DIR_ASSETS=${CODE_DIR}/assets
+DIR_DATASET=/home/data/edufraisse/DATA/LUCIE/${folder_dataset}
+DIR_OUTPUT=/home/data/edufraisse/DATA/LUCIE/perplexity_corpus_open_llm
+
+python ${CODE_DIR}/Bloom-ng-dataset-processing/src/blmrdata/utils/redpajama/worker_parquet.py \
+--dir-input $DIR_DATASET \
+--dir-output $DIR_OUTPUT \
+--path_fasttext_model ${DIR_ASSETS}/fasttext/lid.176.bin \
+--dir_perplexity_models ${DIR_ASSETS}/ccnet_models \
+--language 'fr' \
+--n-processes 72 \
+--flush-freq 1000
