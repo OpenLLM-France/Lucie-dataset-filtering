12534a7ddff77c3582a7e46a75caeb5142856733
Evan DUFRAISSE <evan.dufraisse@cea.fr>
2024-02-22 14:19:26 +0100

    Add script to submit all parquet files for processing
    This commit adds a new script, "submit_all_parquet_wise_en.sh", which allows for the submission of all parquet files in a given input folder for processing. The script takes two arguments: the root output folder and the root input folder. It loops through each dataset in the "datasets" array and executes the "sbatch" command for each parquet file in the corresponding input folder. If a file has already been processed, it skips it. This script will help automate the processing of parquet files.

---
+++ scripts/submit_all_parquet_wise_en.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+root_output_folder=$1
+root_input_folder=$2
+
+
+# Array of dataset names
+datasets=("americanstories")
+    sbatch job_jz.sh "${dataset}"
+done
+# Loop through each dataset and execute sbatch command
+for dataset in "${datasets[@]}"; do
+    echo "Processing $dataset"
+    input_folder="${root_input_folder}/${dataset}"
+    output_folder="${root_output_folder}/${dataset}"
+    for file in "$input_folder"/*.parquet; do
+        filename=$(basename "$file")
+        if [ ! -f "$output_folder/$filename" ]; then
+            sbatch job_jz_parquet_wise_en.sh ${dataset}/${filename}
+        else
+            echo "$filename already processed"
+        fi
+    done
+done
