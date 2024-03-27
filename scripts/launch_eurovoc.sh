93d0d7db9b17500d9af65f50c3fc3045884b0e6d
Evan Dufraisse <edufraisse@gmail.com>
2024-03-27 15:11:27 +0100

    Fix file paths in launch_eurovoc.sh script

--- scripts/launch_eurovoc.sh
+++ scripts/launch_eurovoc.sh
@@ -3,5 +3,5 @@
 langs=('de' 'it' 'en' 'es')
 
 for lang in "${langs[@]}"; do
-    bash ./submit_all_parquet_wise_${lang}.sh /gpfswork/rech/qgz/commun/data/corpus_openllm /gpfswork/rech/ksy/uyc63jm/DATA/corpus_openllm_processed
+    bash ./submit_all_parquet_wise_${lang}.sh /gpfswork/rech/ksy/uyc63jm/DATA/corpus_openllm_processed /gpfswork/rech/qgz/commun/data/corpus_openllm 
 done
